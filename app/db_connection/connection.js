15c4486c37d5d732ab8b4ff5f7458d25c98d8b05
Jordan Toth <jtoth@zynga.com>
2015-03-01 21:49:25 -0800

    Added a database function where you can search for a string contained in a given field instead of an exact match.

--- app/db_connection/connection.js
+++ app/db_connection/connection.js
@@ -1,5 +1,18 @@
 var mysql = require('mysql');
 
+var arrangementFields = [
+    "id",
+    "name",
+    "original_song_year",
+    "quality",
+    "genre",
+    "nickname",
+    "pdf_url",
+    "finale_url",
+    "youtube_url",
+    "arranged_semester_id"
+];
+
 /**
  * Returns a connection object, pass it to the other functions
  * to open/close/get stuff from the db
@@ -17,7 +30,6 @@ function createConnection()
 
 /**
  * Opens a connection, allowing you to use it in querying the database
- *
  */
 function openConnection(connection)
 {
@@ -34,7 +46,7 @@ function openConnection(connection)
 	});
 }
 
-/*
+/**
  * Closes a connection. Call when you're done querying.
  */
 function closeConnection(connection)
@@ -48,29 +60,54 @@ function closeConnection(connection)
  * and passing them to callback(error, rows).
  *
  * @param connection	- a mysql connection object, must be open.
- * @param field			- the field to search the database over.
- * @param value			- the value to search for.
+ * @param field			- the field to find matches in.
+ * @param value			- the value to match.
  * @param callback		- a callback function(err, rows)
  *		@param err 	- any error thrown during the mysql fetch
  * 		@param rows - an array of row objects of the format fieldname:value
  */
-function getArrangement(connection, field, value, callback)
+function getArrangements(connection, field, value, callback)
 {
-	//connection.connect();
-	sql = "SELECT * FROM arrangement WHERE "+field+" = \'"+value+"\'";
+	sql = "SELECT * FROM arrangement WHERE "+field+" = '"+value+"'";
 	connection.query(sql, callback);
-	//connection.end()
 }
+
+/*
+ * Queries the database, returning all arrangements where "field" contains "search"
+ * and passing them to callback(error, rows).
+ *
+ * @param connection	- a mysql connection object, must be open.
+ * @param field			- the field to search the database over.
+ * @param search		- the value to search for.
+ * @param callback		- a callback function(err, rows)
+ *		@param err 	- any error thrown during the mysql fetch
+ * 		@param rows - an array of row objects of the format fieldname:value
+ */
+function searchArrangements(connection, field, value, callback)
+{
+	sql = "SELECT * FROM arrangement WHERE "+field+" LIKE '%"+value+"%'";
+	connection.query(sql, callback);
+}
+
+// TODO: Function that searches both name and nickname, specifically for song lookups by names
+
+// TODO: Finish this. This is what lets you iterate over database entries without loading all at once.
+function getArrangementIterator(connection)
+{
+	//return
+}
+
 
 // Just me testing things out.
 // connection = createConnection();
 // openConnection(connection);
-// getArrangement(connection, "", 1, function(err, rows)
+// searchArrangements(connection, function(err, cols)
 // 	{
-// 		for(i = 0; i < rows.length; i++)
+// 		// console.log(cols);
+// 		var keys = [];
+// 		for(var k in cols)
 // 		{
-// 			row = rows[i];
-// 			console.log(row);
+// 			console.log(cols[k]);
 // 		}
 // 	});
 // closeConnection(connection);
