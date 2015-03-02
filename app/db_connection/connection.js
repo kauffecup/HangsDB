ac752336b2da25c37dfdbf6998c34d198855d98f
Jordan Toth <jtoth@zynga.com>
2015-03-01 18:31:48 -0800

    Got the search over a given field working

--- app/db_connection/connection.js
+++ app/db_connection/connection.js
@@ -1,10 +1,4 @@
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//         host : 'localhost',
-//         user : 'root',
-//         password : '',
-//         database : 'Sage'
-// });
+var mysql = require('mysql');
 
 /**
  * Returns a connection object, pass it to the other functions
@@ -30,7 +24,7 @@ function openConnection(connection)
 	connection.connect(function(err){
 		if(err)
 		{
-			console.err("error connecting: " + err.stack);
+			console.err("Error connecting: " + err.stack);
 			return
 		}
 		else
@@ -43,21 +37,40 @@ function openConnection(connection)
 /*
  * Closes a connection. Call when you're done querying.
  */
-
 function closeConnection(connection)
 {
 	connection.end();
+	console.log("Connection closed");
 }
 
 /*
  * Queries the database, returning all arrangements where "field" matches "value"
  * and passing them to callback(error, rows).
+ *
+ * @param connection	- a mysql connection object, must be open.
+ * @param field			- the field to search the database over.
+ * @param value			- the value to search for.
+ * @param callback		- a callback function(err, rows)
+ *		@param err 	- any error thrown during the mysql fetch
+ * 		@param rows - an array of row objects of the format fieldname:value
  */
-
 function getArrangement(connection, field, value, callback)
 {
 	//connection.connect();
 	sql = "SELECT * FROM arrangement WHERE "+field+" = \'"+value+"\'";
-	connection.query(sql, callback(err, rows));
+	connection.query(sql, callback);
 	//connection.end()
 }
+
+// Just me testing things out.
+// connection = createConnection();
+// openConnection(connection);
+// getArrangement(connection, "", 1, function(err, rows)
+// 	{
+// 		for(i = 0; i < rows.length; i++)
+// 		{
+// 			row = rows[i];
+// 			console.log(row);
+// 		}
+// 	});
+// closeConnection(connection);
