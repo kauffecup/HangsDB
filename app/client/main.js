116d715069d7b9742aa19ccb152f73a7740d4743
Jonathan Kaufman <jondakauf@gmail.com>
2015-03-07 19:09:22 -0500

    convert to a react app! wooo

--- app/client/main.js
+++ app/client/main.js
@@ -1,1 +1,10 @@
-require('react');
+var JSX = require('node-jsx'),
+    React = require('react');
+
+React.render(
+  <div className="window">
+    <h1>Sage</h1>
+    <p>welcome to Sage.</p>
+  </div>,
+  document.getElementById('body')
+);
