855ebb6b590966b7f7abbd9afcc89ac41e629683
Jonathan Kaufman <jondakauf@gmail.com>
2015-03-14 11:11:18 -0400

    make sure array values... are arrays not strings

--- app/routes.js
+++ app/routes.js
@@ -35,11 +35,37 @@ router.post('/upload', function (req, res) {
 
   var body = req && req.body;
   if (body) {
-    connection.addAllToArrangement(dbconnection, body.name, body.opb, body.year, body.arrangedSemester,
-      body.quality, body.reception, body.genre, body.arrType, body.nickname, stringToBoolean(body.hasSyllables),
-      '', '', body.youtubeLink, body.pitchBlown, body.difficulty, stringToBoolean(body.hasChoreo), body.soloRange,
-      body.notes, body.key, '', stringToBoolean(body.isActive), body.numParts, body.arrangedby, body.semestersIn,
-      body.soloists, body.mds, body.concertsIn, function (err, result) {
+    connection.addAllToArrangement(
+      dbconnection,
+      body.name,
+      body.opb,
+      body.year,
+      body.arrangedSemester,
+      body.quality,
+      body.reception,
+      body.genre,
+      body.arrType,
+      body.nickname,
+      stringToBoolean(body.hasSyllables),
+      '',  // pdf URL, blank for now
+      '',  // finale URL, blank for now
+      body.youtubeLink,
+      body.pitchBlown,
+      body.difficulty,
+      stringToBoolean(body.hasChoreo),
+      body.soloRange,
+      body.notes,
+      body.key,
+      '',  // song URL, blank for now
+      stringToBoolean(body.isActive),
+      body.numParts,
+      // these have to be passed in as arrays, not as strings
+      [].concat(body.arrangedby),
+      [].concat(body.semestersIn),
+      [].concat(body.soloists),
+      [].concat(body.mds),
+      [].concat(body.concertsIn),
+      function (err, result) {
         if (err) {
           res.status(502);
           res.json(err);
