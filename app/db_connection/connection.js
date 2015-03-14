7f97186672761385f3abb60d2b6d6ca113855fa9
Jordan Toth <jtoth@zynga.com>
2015-03-14 00:02:56 -0700

    Finished the add all function. More like the adderall function because fuck working late post college am I right?

--- app/db_connection/connection.js
+++ app/db_connection/connection.js
@@ -62,15 +62,17 @@ module.exports = {
   	});
   },
 
-  addAllToArrangement: function (connection, name, artist_name, original_song_year, arranged_semester_name, quality, reception, genre, arrangement_type_id, nickname, has_syllables, pdf_url, finale_url, youtube_url, blown_pitch_id, difficulty, has_choreo, solo_voice_part_id, notes, song_key, recording_url, active, number_of_parts, arranger_array, semester_array, soloist_array, director_array, concert_array, callback)
-  {
+// WARNING: DO NOT CALL CLOSECONNECTION() RIGHT AFTER THIS. WAIT AT LEAST A SECOND.
+//OTHERWISE MANY-TO-MANY TABLES WILL NOT BE UPDATED USING THE NEW ARRANGEMENT ID AS THE CALLBACK FUNCTION WILL NOT BE FINISHED.
+addAllToArrangement: function (connection, name, artist_name, original_song_year, arranged_semester_name, quality, reception, genre, arrangement_type_id, nickname, has_syllables, pdf_url, finale_url, youtube_url, blown_pitch_id, difficulty, has_choreo, solo_voice_part_id, notes, song_key_id, recording_url, active, number_of_parts, arranger_array, semester_array, soloist_array, director_array, concert_array, callback)
+{
   	// get ready for the mysql query of your life.
   	// solo_voice_part_id and arrangement_type_id are going to be straight IDs.
   	sql = "INSERT IGNORE INTO artist SET name = '"+artist_name+"'";
-    connection.query(sql, function(err, rows) {
+    connection.query(sql, function(err, result) {
     	if(err)
     	{
-    		console.log("Error inserting artist: "+err.stack);
+    		console.log("Error or duplicate inserting artist: "+err.stack);
     	}
     	else 
     	{
@@ -79,10 +81,10 @@ module.exports = {
     });
 
     sql = "INSERT IGNORE INTO semester SET name = '"+arranged_semester_name+"'";
-    connection.query(sql, function(err, rows) {
+    connection.query(sql, function(err, result) {
     	if(err)
     	{
-    		console.log("Error inserting semester: "+err.stack);
+    		console.log("Error or duplicate inserting semester: "+err.stack);
     	}
     	else 
     	{
@@ -90,29 +92,184 @@ module.exports = {
     	}
     });
 
-   //  sql = "INSERT INTO arrangement"
+    sql = "INSERT INTO arrangement (name, original_song_year, quality, genre, nickname, pdf_url, finale_url, youtube_url, arranged_semester_id, reception, notes, difficulty, recording_url, song_key, pitch_blown, number_of_parts, solo_voice_part_id, has_choreo, active, has_syllables, artist_id, arrangement_type_id) VALUES(";
+    sql += "'"+name+"', ";
+    sql += original_song_year+", ";
+    sql += quality+", ";
+    sql += "'"+genre+"', ";
+    sql += "'"+nickname+"', ";
+    sql += "'"+pdf_url+"', ";
+    sql += "'"+finale_url+"', ";
+    sql += "'"+youtube_url+"', ";
+    sql += "(SELECT id FROM semester WHERE name = '"+arranged_semester_name+"'), ";
+    sql += reception+", "
+    sql += "'"+notes+"', ";
+    sql += difficulty+", ";
+    sql += "'"+recording_url+"', ";
+    sql += song_key_id+", ";
+    sql += blown_pitch_id+", ";
+    sql += number_of_parts+", ";
+    sql += solo_voice_part_id+", ";
+    sql += has_choreo+", ";
+    sql += active+", ";
+    sql += has_syllables+", ";
+ 	sql += "(SELECT id FROM artist WHERE name = '"+artist_name+"'), ";
+ 	sql += arrangement_type_id+")";
+	console.log(sql);
+	connection.query(sql, function(err, result) {
+		if(err)
+		{
+			console.log("Error inserting arrangement ");
+		}
+		else
+		{
+			console.log("Inserted arrangement");
+			var newArrangementId = result.insertId;
+			for(i=0; i<arranger_array.length; i++)
+			{
+				arranger_name = arranger_array[i];
 
-  	// for(i=0; i<arranger_array.length; i++)
-  	// {
-  	// 	arranger = arranger_array[i];
-  	// }
-  	// for(i=0; i<semester_array.length; i++)
-  	// {
-  	// 	semester = semester_array[i];
-  	// }
-  	// for(i=0; i<soloist_array.length; i++)
-  	// {
-  	// 	soloist = soloist_array[i];
-  	// }
-  	// for(i=0; i<director_array.length; i++)
-  	// {
-  	// 	director = director_array[i];
-  	// }
-  	// for(i=0; i<concert_array.length; i++)
-  	// {
-  	// 	concert = concert_array[i];
-  	// }
-  },
+			    sql = "INSERT IGNORE INTO hangover SET name = '"+arranger_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting arranger: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted arranger");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_arranger (arrangement_id, hangover_id) VALUES("+newArrangementId+", (SELECT id FROM hangover WHERE name = '"+arranger_name+"')";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting arranger junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted arranger junction");
+		    		}
+    			});
+			}
+			for(i=0; i<semester_array.length; i++)
+			{
+				semester_name = semester_array[i];
+
+			    sql = "INSERT IGNORE INTO semester SET name = '"+semester_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting semester: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted semester");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_semester (arrangement_id, semester_id) VALUES("+newArrangementId+", (SELECT id FROM semester WHERE name = '"+semester_name+"'))";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting semester junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted semester junction");
+		    		}
+    			});
+			}
+			for(i=0; i<soloist_array.length; i++)
+			{
+				soloist_name = soloist_array[i];
+
+				sql = "INSERT IGNORE INTO hangover SET name = '"+soloist_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting soloist: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted soloist");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_soloist (arrangement_id, hangover_id) VALUES("+newArrangementId+", (SELECT id FROM hangover WHERE name = '"+soloist_name+"'))";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting soloist junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted soloist junction");
+		    		}
+    			});
+
+			}
+			for(i=0; i<director_array.length; i++)
+			{
+				director_name = director_array[i];
+
+				sql = "INSERT IGNORE INTO hangover SET name = '"+director_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting director: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted director");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_director (arrangement_id, hangover_id) VALUES("+newArrangementId+", (SELECT id FROM hangover WHERE name = '"+director_name+"'))";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting director junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted director junction");
+		    		}
+    			});
+			}
+			for(i=0; i<concert_array.length; i++)
+			{
+				concert_name = concert_array[i];
+
+				sql = "INSERT IGNORE INTO concert SET name = '"+concert_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting concert: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted concert");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_concert (arrangement_id, concert_id) VALUES("+newArrangementId+", (SELECT id FROM concert WHERE name = '"+concert_name+"'))";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting concert junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted concert junction");
+		    		}
+    			});
+			}
+		}
+		callback(err, result);
+	});
+},
 
   /**
    * Opens a connection, allowing you to use it in querying the database
--- app/db_connection/test.js
+++ app/db_connection/test.js
@@ -32,15 +32,15 @@ function closeConnection(connection)
 }
 
 
-function addAllToArrangement (connection, name, artist_name, original_song_year, arranged_semester_name, quality, reception, genre, arrangement_type_id, nickname, has_syllables, pdf_url, finale_url, youtube_url, blown_pitch_id, difficulty, has_choreo, solo_voice_part_id, notes, song_key, recording_url, active, number_of_parts, arranger_array, semester_array, soloist_array, director_array, concert_array, callback)
+function addAllToArrangement (connection, name, artist_name, original_song_year, arranged_semester_name, quality, reception, genre, arrangement_type_id, nickname, has_syllables, pdf_url, finale_url, youtube_url, blown_pitch_id, difficulty, has_choreo, solo_voice_part_id, notes, song_key_id, recording_url, active, number_of_parts, arranger_array, semester_array, soloist_array, director_array, concert_array, callback)
 {
   	// get ready for the mysql query of your life.
   	// solo_voice_part_id and arrangement_type_id are going to be straight IDs.
   	sql = "INSERT IGNORE INTO artist SET name = '"+artist_name+"'";
-    connection.query(sql, function(err, rows) {
+    connection.query(sql, function(err, result) {
     	if(err)
     	{
-    		console.log("Error inserting artist: "+err.stack);
+    		console.log("Error or duplicate inserting artist: "+err.stack);
     	}
     	else 
     	{
@@ -49,10 +49,10 @@ function addAllToArrangement (connection, name, artist_name, original_song_year,
     });
 
     sql = "INSERT IGNORE INTO semester SET name = '"+arranged_semester_name+"'";
-    connection.query(sql, function(err, rows) {
+    connection.query(sql, function(err, result) {
     	if(err)
     	{
-    		console.log("Error inserting semester: "+err.stack);
+    		console.log("Error or duplicate inserting semester: "+err.stack);
     	}
     	else 
     	{
@@ -60,42 +60,190 @@ function addAllToArrangement (connection, name, artist_name, original_song_year,
     	}
     });
 
-    // sql = "INSERT INTO arrangement SET name = name"
-    // connection.query(sql, function(err, rows) {
-    // 	if(err)
-    // 	{
-    // 		console.err("Error inserting arrangement: "+err.stack);
-    // 	}
-    // });
+    sql = "INSERT INTO arrangement (name, original_song_year, quality, genre, nickname, pdf_url, finale_url, youtube_url, arranged_semester_id, reception, notes, difficulty, recording_url, song_key, pitch_blown, number_of_parts, solo_voice_part_id, has_choreo, active, has_syllables, artist_id, arrangement_type_id) VALUES(";
+    sql += "'"+name+"', ";
+    sql += original_song_year+", ";
+    sql += quality+", ";
+    sql += "'"+genre+"', ";
+    sql += "'"+nickname+"', ";
+    sql += "'"+pdf_url+"', ";
+    sql += "'"+finale_url+"', ";
+    sql += "'"+youtube_url+"', ";
+    sql += "(SELECT id FROM semester WHERE name = '"+arranged_semester_name+"'), ";
+    sql += reception+", "
+    sql += "'"+notes+"', ";
+    sql += difficulty+", ";
+    sql += "'"+recording_url+"', ";
+    sql += song_key_id+", ";
+    sql += blown_pitch_id+", ";
+    sql += number_of_parts+", ";
+    sql += solo_voice_part_id+", ";
+    sql += has_choreo+", ";
+    sql += active+", ";
+    sql += has_syllables+", ";
+ 	sql += "(SELECT id FROM artist WHERE name = '"+artist_name+"'), ";
+ 	sql += arrangement_type_id+")";
+	console.log(sql);
+	connection.query(sql, function(err, result) {
+		if(err)
+		{
+			console.log("Error inserting arrangement ");
+		}
+		else
+		{
+			console.log("Inserted arrangement");
+			var newArrangementId = result.insertId;
+			for(i=0; i<arranger_array.length; i++)
+			{
+				arranger_name = arranger_array[i];
 
-  	// for(i=0; i<arranger_array.length; i++)
-  	// {
-  	// 	arranger = arranger_array[i];
-  	// }
-  	// for(i=0; i<semester_array.length; i++)
-  	// {
-  	// 	semester = semester_array[i];
-  	// }
-  	// for(i=0; i<soloist_array.length; i++)
-  	// {
-  	// 	soloist = soloist_array[i];
-  	// }
-  	// for(i=0; i<director_array.length; i++)
-  	// {
-  	// 	director = director_array[i];
-  	// }
-  	// for(i=0; i<concert_array.length; i++)
-  	// {
-  	// 	concert = concert_array[i];
-  	// }
+			    sql = "INSERT IGNORE INTO hangover SET name = '"+arranger_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting arranger: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted arranger");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_arranger (arrangement_id, hangover_id) VALUES("+newArrangementId+", (SELECT id FROM hangover WHERE name = '"+arranger_name+"')";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting arranger junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted arranger junction");
+		    		}
+    			});
+			}
+			for(i=0; i<semester_array.length; i++)
+			{
+				semester_name = semester_array[i];
+
+			    sql = "INSERT IGNORE INTO semester SET name = '"+semester_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting semester: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted semester");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_semester (arrangement_id, semester_id) VALUES("+newArrangementId+", (SELECT id FROM semester WHERE name = '"+semester_name+"'))";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting semester junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted semester junction");
+		    		}
+    			});
+			}
+			for(i=0; i<soloist_array.length; i++)
+			{
+				soloist_name = soloist_array[i];
+
+				sql = "INSERT IGNORE INTO hangover SET name = '"+soloist_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting soloist: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted soloist");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_soloist (arrangement_id, hangover_id) VALUES("+newArrangementId+", (SELECT id FROM hangover WHERE name = '"+soloist_name+"'))";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting soloist junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted soloist junction");
+		    		}
+    			});
+
+			}
+			for(i=0; i<director_array.length; i++)
+			{
+				director_name = director_array[i];
+
+				sql = "INSERT IGNORE INTO hangover SET name = '"+director_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting director: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted director");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_director (arrangement_id, hangover_id) VALUES("+newArrangementId+", (SELECT id FROM hangover WHERE name = '"+director_name+"'))";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting director junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted director junction");
+		    		}
+    			});
+			}
+			for(i=0; i<concert_array.length; i++)
+			{
+				concert_name = concert_array[i];
+
+				sql = "INSERT IGNORE INTO concert SET name = '"+concert_name+"'";
+				connection.query(sql, function(err, result) {
+			    	if(err)
+			    	{
+			    		console.log("Error or duplicate inserting concert: "+err.stack);
+			    	}
+			    	else 
+			    	{
+			    		console.log("Inserted concert");
+			    	}
+		    	});
+
+				sql = "INSERT IGNORE INTO arrangement_concert (arrangement_id, concert_id) VALUES("+newArrangementId+", (SELECT id FROM concert WHERE name = '"+concert_name+"'))";
+    			connection.query(sql, function(err, result) {
+		    		if(err)
+		    		{
+		    			console.log("Error or duplicate inserting concert junction: "+err.stack);
+		    		}
+		    		else 
+		    		{
+		    			console.log("Inserted concert junction");
+		    		}
+    			});
+			}
+		}
+		callback(err, result);
+	});
 }
 
 
   // Just me testing things out.
 connection = createConnection();
 openConnection(connection);
-addAllToArrangement(connection, "dummy1","dummy2","dummy3","dummy4","dummy5","dummy6","dummy7","dummy8","dummy9","dummy10","dummy11","dummy12","dummy13","dummy14","dummy15","dummy16","dummy17","dummy18","dummy19","dummy20","dummy21","dummy22","dummy23","dummy24","dummy25","dummy26","dummy27", function(){
+addAllToArrangement(connection, "dummy name","dummier artist",1901,"Fall 1902",10,1,"dummy genre",1,"DUM",true,"c/dum","d/dum","e/dum",1,10,1,1,"dummy notes",1,"f/dum",1,4,["Warren Lowell","Jordan Toth", "H. Michael Newman"],["Warren Lowell","Jordan Toth", "H. Michael Newman"],["Warren Lowell","Jordan Toth", "H. Michael Newman"],["Warren Lowell","Jordan Toth", "H. Michael Newman"],["Warren Lowell","Jordan Toth", "H. Michael Newman"], function(){
 	console.log("Here");
 });
-closeConnection(connection);
 
