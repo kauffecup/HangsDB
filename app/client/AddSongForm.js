7ca06f1602ff5cfa6fa9af5b6db64cf9c876ddec
Jonathan Kaufman <jondakauf@gmail.com>
2015-03-14 00:47:55 -0400

    simplify upload POST callback

--- app/client/AddSongForm.js
+++ app/client/AddSongForm.js
@@ -127,20 +127,10 @@ module.exports = React.createClass({
         'Content-Length': postData.length
       }
     };
-
-    var req = http.request(options, function(res) {
-      console.log('STATUS: ' + res.statusCode);
-      console.log('HEADERS: ' + JSON.stringify(res.headers));
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
-      });
-    });
-
+    var req = http.request(options, function (res) {});
     req.on('error', function(e) {
       console.log('problem with request: ' + e.message);
     });
-
     // write data to request body
     req.write(postData);
     req.end();
