ca11a3532e49532ec91d56a65c48ab7670762ffb
Jordan Toth <jtoth@zynga.com>
2015-03-13 20:45:27 -0700

    Fixed function header

--- app/db_connection/connection.js
+++ app/db_connection/connection.js
@@ -62,11 +62,48 @@ module.exports = {
   	});
   },
 
-  addAllToArrangement: function (name, artist_id, original_song_year, arranged_semester_id, quality, reception, genre, arrangement_type_id, nickname, has_syllables, pdf_url, finale_url, youtube_url, pitch_blown, difficulty, has_choreo, solo_voice_part_id, notes, song_key, recording_url, active, number_of_parts, arranger_array, semester_array, soloist_array, director_array, concert_array)
+  addAllToArrangement: function (connection, name, artist_name, original_song_year, arranged_semester_name, quality, reception, genre, arrangement_type_id, nickname, has_syllables, pdf_url, finale_url, youtube_url, blown_pitch_id, difficulty, has_choreo, solo_voice_part_id, notes, song_key, recording_url, active, number_of_parts, arranger_array, semester_array, soloist_array, director_array, concert_array, callback)
   {
   	// get ready for the mysql query of your life.
-  	//arranger_array, semester_array, soloist_array, director_array, concert_array
-  	//for(i=0; i<fieldArray.length; i++)
+  	// solo_voice_part_id and arrangement_type_id are going to be straight IDs.
+  	sql = "INSERT IGNORE INTO artist SET name = "+artist_name;
+    connection.query(sql, function() {
+    	if(err)
+    	{
+    		console.err("Error inserting artist: "+err.stack);
+    	}
+    });
+
+    sql = "INSERT IGNORE INTO semester SET name = "+arranged_semester_name;
+    connection.query(sql, function() {
+    	if(err)
+    	{
+    		console.err("Error inserting semester: "+err.stack);
+    	}
+    });
+
+   //  sql = "INSERT INTO arrangement"
+
+  	// for(i=0; i<arranger_array.length; i++)
+  	// {
+  	// 	arranger = arranger_array[i];
+  	// }
+  	// for(i=0; i<semester_array.length; i++)
+  	// {
+  	// 	semester = semester_array[i];
+  	// }
+  	// for(i=0; i<soloist_array.length; i++)
+  	// {
+  	// 	soloist = soloist_array[i];
+  	// }
+  	// for(i=0; i<director_array.length; i++)
+  	// {
+  	// 	director = director_array[i];
+  	// }
+  	// for(i=0; i<concert_array.length; i++)
+  	// {
+  	// 	concert = concert_array[i];
+  	// }
   },
 
   /**
--- app/db_connection/test.js
+++ app/db_connection/test.js
@@ -31,20 +31,61 @@ function closeConnection(connection)
     console.log("Connection closed");
 }
 
+
+function addAllToArrangement (name, artist_name, original_song_year, arranged_semester_name, quality, reception, genre, arrangement_type_id, nickname, has_syllables, pdf_url, finale_url, youtube_url, blown_pitch_id, difficulty, has_choreo, solo_voice_part_id, notes, song_key, recording_url, active, number_of_parts, arranger_array, semester_array, soloist_array, director_array, concert_array)
+{
+  	// get ready for the mysql query of your life.
+  	// solo_voice_part_id and arrangement_type_id are going to be straight IDs.
+  	sql = "INSERT IGNORE INTO artist SET name = "+artist_name;
+    connection.query(sql, function {
+    	if(err)
+    	{
+    		console.err("Error inserting artist: "+err.stack);
+    	}
+    });
+
+    sql = "INSERT IGNORE INTO semester SET name = "+arranged_semester_name;
+    connection.query(sql, function {
+    	if(err)
+    	{
+    		console.err("Error inserting semester: "+err.stack);
+    	}
+    });
+
+    sql = "INSERT INTO arrangement SET name = name"
+    connection.query(sql, function {
+    	if(err)
+    	{
+    		console.err("Error inserting arrangement: "+err.stack);
+    	}
+    });
+
+  	// for(i=0; i<arranger_array.length; i++)
+  	// {
+  	// 	arranger = arranger_array[i];
+  	// }
+  	// for(i=0; i<semester_array.length; i++)
+  	// {
+  	// 	semester = semester_array[i];
+  	// }
+  	// for(i=0; i<soloist_array.length; i++)
+  	// {
+  	// 	soloist = soloist_array[i];
+  	// }
+  	// for(i=0; i<director_array.length; i++)
+  	// {
+  	// 	director = director_array[i];
+  	// }
+  	// for(i=0; i<concert_array.length; i++)
+  	// {
+  	// 	concert = concert_array[i];
+  	// }
+}
+
+
   // Just me testing things out.
 connection = createConnection();
-// var fields = new Array("id", "pdf_url");
-// var values = new Array(1, "NULL");
 openConnection(connection);
-// getNameFromId(connection, 1, function(err, rows)
-// 	{
-// 		for(i=0; i<rows.length; i++)
-// 		{
-// 			console.log(rows[i]['name']);
-// 		}
-// 	});
+addAllToArrangement("dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy");
 closeConnection(connection);
-setTimeout(function(){
-    openConnection(connection);
-},5000);
 
